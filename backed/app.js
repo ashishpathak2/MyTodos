00750e370f591e99db9d9d49d84cbc765a07d3e0
ashishpathak2 <pathak1420@gmail.com>
2024-04-30 23:33:55 +0530

    changes

--- backed/app.js
+++ backed/app.js
@@ -25,7 +25,7 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors({
-  origin:["https://my-todos-1koj.vercel.app/"],
+  origin:["https://my-todos-1koj.vercel.app"],
   methods:["GET","POST","DELETE","PUT"],
   credentials:true
 }));
