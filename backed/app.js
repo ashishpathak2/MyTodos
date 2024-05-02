42262bd148edaf1f7cfc6436a53d4333a3efd657
ashishpathak2 <pathak1420@gmail.com>
2024-05-02 22:33:16 +0530

    changes

--- backed/app.js
+++ backed/app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.options('*', cors()) 
 app.use(cors({
   origin:["https://my-todos-1koj.vercel.app"],
-  methods:["GET","POST","DELETE","PUT"],
+  methods:["GET","POST","DELETE","PUT","OPTIONS"],
   credentials:true
 }));
 app.use(cookieParser());
