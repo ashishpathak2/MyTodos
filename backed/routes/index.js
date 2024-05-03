da682de96fcd2e084cb585ab26895783fdc67343
ashishpathak2 <pathak1420@gmail.com>
2024-05-03 23:03:58 +0530

    changes

--- backed/routes/index.js
+++ backed/routes/index.js
@@ -11,7 +11,7 @@ router.get('/',function (req, res, next) {
 });
 
 
-router.get('/todo',isLoggedIn, async function (req, res) {
+router.get('/todo', async function (req, res) {
   await todoItems.find({CreatedByuserId : req.session.passport.user._id}).
     then((result) => {
       res.send(result).status(200)
