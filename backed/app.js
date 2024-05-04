853d718d9ea827afe17b095ea8ecd38afb6ad9a5
ashishpathak2 <pathak1420@gmail.com>
2024-05-04 14:02:57 +0530

    changes

--- backed/app.js
+++ backed/app.js
@@ -37,6 +37,11 @@ app.use(Session({
   resave:false,
   saveUninitialized:false,
   secret:"heyashishhere",
+  cookie:{
+    secure:true,
+    maxAge:24*60*60*1000,
+    httpOnly:false,
+  },
   store:mongostore.create({
     mongoUrl:"mongodb://mongo:XVXaxLcCjsfqOJKSwaGuUNWGiTOkYzJR@monorail.proxy.rlwy.net:15015",
   }),
