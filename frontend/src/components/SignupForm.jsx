1dab1781288f0d86f88cf21b9db83acd9398958d
ashishpathak2 <pathak1420@gmail.com>
2024-05-05 21:58:40 +0530

    changes

--- frontend/src/components/SignupForm.jsx
+++ frontend/src/components/SignupForm.jsx
@@ -33,9 +33,14 @@ const Addform = ({ }) => {
     })
   }
 
+  const googleHandlerfunc = async ()=> {
+    const val = await googleHandler()
+    console.log(val);
+  }
+
   const googleHandler = async ()=>{
     window.open("https://mytodosapp-6h9w.onrender.com/users/auth","_self");
-    return ;
+    return "hello";
     // loggedUserName();
   }
 
@@ -202,7 +207,7 @@ const Addform = ({ }) => {
                 </div> */}
                    <div className="flex items-center justify-center">
                   <button className="bg-white w-60 flex items-center justify-center gap-2 p-2 text-center rounded-sm hover:scale-110 transition ease-in-out "
-                    onClick={() => googleHandler()}><FcGoogle size="1.8rem" />Continue with Google</button>
+                    onClick={() => googleHandlerfunc()}><FcGoogle size="1.8rem" />Continue with Google</button>
                 </div>
 
 
