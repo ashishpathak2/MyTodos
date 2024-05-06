8f086bb5d390438beffefbed0ab46a29d787af1b
ashishpathak2 <pathak1420@gmail.com>
2024-05-06 20:05:12 +0530

    changes

--- backed/app.js
+++ backed/app.js
@@ -26,15 +26,15 @@ app.use(cors({
   credentials:true
 }));
 app.use(cookieParser());
-app.set("trust proxy", 1);
+// app.set("trust proxy", 1);
 app.use(Session({
   resave:false,
   saveUninitialized:false,
   secret:"heyashishhere",
   cookie:{
-    secure:true,
-    httpOnly: true ,
-    sameSite:"none",
+    secure:false,
+    // httpOnly: true ,
+    // sameSite:"none",
     expires:24*60*60*1000,
   },
   // store:monogoStore.create({
