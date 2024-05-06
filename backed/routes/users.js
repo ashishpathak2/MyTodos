e133595edf8de6389935635006aaf26983ee599e
ashishpathak2 <pathak1420@gmail.com>
2024-05-06 21:41:34 +0530

    changes

--- backed/routes/users.js
+++ backed/routes/users.js
@@ -53,8 +53,8 @@ router.get("/auth/google",passport.authenticate("google",{
 
 
 router.get("/authUserName",function(req,res){
-  console.log(req.session.passport.user);
-  res.send(req.session.passport.user.username)
+  console.log(req.sessionStore);
+  // res.send(req.session.passport.user.username)
   // res.cookie("username" ,req.session.passport.user.username)
   // res.redirect("https://my-todos-1koj.vercel.app") 
 })
