ec11561106824720bfba1e0312eb410d44b690b0
ashishpathak2 <pathak1420@gmail.com>
2024-05-07 22:41:42 +0530

    changes

--- frontend/src/components/SignupForm.jsx
+++ frontend/src/components/SignupForm.jsx
@@ -38,9 +38,9 @@ const Addform = ({ }) => {
     })
   }
 
-  // useEffect(() => {
-  //   googleloggedUser()
-  // }, [])
+  useEffect(() => {
+    googleloggedUser()
+  }, [])
 
   const handleClick = () => {
     if (!userData) setIsOpen(true);
@@ -58,7 +58,6 @@ const Addform = ({ }) => {
 
 
   useEffect(() => {
-    googleloggedUser();
     if (localStorage.getItem("loggedInUser")) {
       setuserData(localStorage.getItem("loggedInUser"))
     }
