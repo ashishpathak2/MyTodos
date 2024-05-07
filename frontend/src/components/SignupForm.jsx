4917c55de07432effec4c76b9a9a85c8de72087b
ashishpathak2 <pathak1420@gmail.com>
2024-05-07 18:38:47 +0530

    changes

--- frontend/src/components/SignupForm.jsx
+++ frontend/src/components/SignupForm.jsx
@@ -49,7 +49,7 @@ const Addform = ({ }) => {
   if(newWindow){
     newWindow.onload = function() {
         loggedUserName();
-        window.location.href ="https://my-todos-1koj.vercel.app" 
+        location.reload();
     };
 } else {
     console.error("Failed to open window.");
