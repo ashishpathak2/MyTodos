4bc919d5292a1ec3f0fb60760defc120f81c513a
ashishpathak2 <pathak1420@gmail.com>
2024-05-07 17:57:32 +0530

    changes

--- frontend/src/components/SignupForm.jsx
+++ frontend/src/components/SignupForm.jsx
@@ -44,7 +44,7 @@ const Addform = ({ }) => {
   // }
 
   const googleHandler = ()=>{
-  var newWindow = window.open("https://mytodosapp-6h9w.onrender.com/users/auth")
+  var newWindow = window.open("https://mytodosapp-6h9w.onrender.com/users/auth","_parent")
   if(newWindow){
     newWindow.onload = function() {
         loggedUserName();
