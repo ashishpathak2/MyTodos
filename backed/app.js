0afc045a40d5da043d41658a789c099a9b5dd9f7
ashishpathak2 <pathak1420@gmail.com>
2024-05-07 13:22:43 +0530

    changes

--- backed/app.js
+++ backed/app.js
@@ -26,7 +26,7 @@ app.use(cors({
   credentials:true
 }));
 app.use(cookieParser());
-app.set("trust proxy", 1);
+// app.set("trust proxy", 1);
 app.use(Session({
   resave:false,
   saveUninitialized:false,
