9393b32c32ab979aa9365063b98fa4f450f14cad
ashishpathak2 <pathak1420@gmail.com>
2024-05-07 22:53:45 +0530

    changes

--- frontend/src/components/SignupForm.jsx
+++ frontend/src/components/SignupForm.jsx
@@ -38,9 +38,9 @@ const Addform = ({ }) => {
     })
   }
 
-  // useEffect(() => {
-  //   googleloggedUser()
-  // }, [])
+  useEffect(() => {
+    googleloggedUser()
+  }, [])
 
   const handleClick = () => {
     if (!userData) setIsOpen(true);
@@ -224,7 +224,7 @@ const Addform = ({ }) => {
 
               <p className='text-center text-white pb-2'>OR</p>
               <div className="flex items-center justify-center">
-                <a onClick={()=> googleloggedUser() } className="bg-white w-full flex items-center justify-center gap-2 p-2 text-center rounded-sm hover:scale-110 transition ease-in-out "
+                <a className="bg-white w-full flex items-center justify-center gap-2 p-2 text-center rounded-sm hover:scale-110 transition ease-in-out "
                   href="https://mytodosapp-6h9w.onrender.com/users/auth"><FcGoogle size="1.8rem" />Continue with Google</a>
               </div>
 
