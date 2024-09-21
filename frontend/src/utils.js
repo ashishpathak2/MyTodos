94952724a8a683d5e8f848a7c947d629783f92cc
ashishpathak2 <pathak1420@gmail.com>
2024-09-21 13:44:23 +0530

    fix

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,3 +1,3 @@
 // export const baseUrl = "https://mytodos-3gmr.onrender.com"
 // export const baseUrl = "http://localhost:3000"
-export const baseUrl = "http://mytodos-env.eba-epb83vmg.ap-south-1.elasticbeanstalk.com"
+export const baseUrl = "https://mytodos.site"
