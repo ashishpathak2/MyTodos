bbf91a5b10b6ee51ea10a8fc5e2fa5ebd8147731
ashishpathak2 <pathak1420@gmail.com>
2024-11-05 14:42:41 +0530

    fix

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,3 +1,3 @@
-// export const baseUrl = "https://mytodos-3gmr.onrender.com"
+export const baseUrl = "https://mytodos-4ix4.onrender.com"
 // export const baseUrl = "http://localhost:3000"
-export const baseUrl = "https://mytodos.site"
+// export const baseUrl = "https://mytodos.site"
